5f9ac0f720f197e3ae6990e043abb6ace2ba1c43
Tharindu Madushan <tharindumpathirana96@gmail.com>
2023-10-15 13:45:50 +0530

    implement schema.sql and generate schema

--- src/main/java/lk/ijse/dep11/pos/db/SingleConnectionDataSource.java
+++ src/main/java/lk/ijse/dep11/pos/db/SingleConnectionDataSource.java
@@ -1,33 +1,44 @@
 package lk.ijse.dep11.pos.db;
 
-import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.Properties;
 
 public class SingleConnectionDataSource {
-    private  static SingleConnectionDataSource instance;
-    private final Connection connection = null;
+    private static SingleConnectionDataSource instance;
+    private final Connection connection;
 
-    private SingleConnectionDataSource() throws IOException {
-        Properties properties = new Properties();
-        properties.load(this.getClass().getResourceAsStream("/Application.properties"));
-        String url = properties.getProperty("app.datasource.url");
-        String username = properties.getProperty("app.datasource.username");
-        String password = properties.getProperty("app.datasource.password");
+    private SingleConnectionDataSource() {
         try {
-            Connection connection = DriverManager.getConnection(url, username, password);
-        } catch (SQLException e) {
+            Properties properties = new Properties();
+            properties.load(this.getClass().getResourceAsStream("/Application.properties"));
+            String username = properties.getProperty("app.datasource.username");
+            String password = properties.getProperty("app.datasource.password");
+            String url = properties.getProperty("app.datasource.url");
+            connection = DriverManager.getConnection(url, username, password);
+            generateSchema();
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        
-    }
-    public static SingleConnectionDataSource getInstance() throws IOException {
-        return instance==null ? instance=new SingleConnectionDataSource() : instance;
     }
 
-    public Connection getConnection(){
+    public static SingleConnectionDataSource getInstance() {
+        return (instance == null) ? (instance = new SingleConnectionDataSource()) : instance;
+    }
+
+    private void generateSchema() throws Exception {
+        URL url = getClass().getResource("/schema.sql");
+        Path path = Paths.get(url.toURI());
+        String dbScript = Files.readAllLines(path).stream()
+                .reduce((prevLine, currentLine) -> prevLine + currentLine).get();
+        connection.createStatement().execute(dbScript);
+    }
+
+    public Connection getConnection() {
         return connection;
     }
 }
